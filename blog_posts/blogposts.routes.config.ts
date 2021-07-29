4463872074f81ce194ab50b464192058fd26fe73
Nayanjeet Saikia <65288687+Neyen108@users.noreply.github.com>
2021-07-29 22:34:43 +0530

    remove unnecessary comments

--- blog_posts/blogposts.routes.config.ts
+++ blog_posts/blogposts.routes.config.ts
@@ -1,5 +1,9 @@
 import { CommonRoutesConfig } from "../common/common.routes.config";
 import express from "express";
+import jwtMiddleware from "../auth/middleware/jwt.middleware";
+import blogpostsController from "./controllers/blogposts.controller";
+import permissionMiddleware from "../common/middleware/common.permission.middleware";
+import { PermissionFlag } from "../common/middleware/common.permissionflag.enum";
 
 export class BlogpostsRoutes extends CommonRoutesConfig {
     constructor(app: express.Application) {
@@ -7,6 +11,18 @@ export class BlogpostsRoutes extends CommonRoutesConfig {
     }
 
     configureRoutes(): express.Application {
+        //have to be logged in for this route
+        this.app
+            .route("/blog")
+            .all(jwtMiddleware.validJWTNeeded)
+            .get(
+                permissionMiddleware.permissionFlagRequired(
+                    PermissionFlag.READ_PERMISSION
+                ),
+                blogpostsController.listBlogposts
+            )
+            .post();
+
         return this.app;
     }
 }
